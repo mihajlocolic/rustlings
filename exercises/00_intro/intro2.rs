dca427e8af16a7675d4452b3faa5650e90b1ff3a
Mihajlo Colic <colicmihajlo@gmail.com>
2024-04-10 20:22:20 +0200

    Solved the intro and variable exercises

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -5,8 +5,10 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
+<<<<<<< HEAD:exercises/00_intro/intro2.rs
     printline!("Hello there!")
+=======
+    println!("Hello!");
+>>>>>>> 4cddc6e (Solved the intro and variable exercises):exercises/intro/intro2.rs
 }
